9200b2913c66b1297d2063dd3c4417c20440bab3
dessna12 <92819503+dessna12@users.noreply.github.com>
2022-12-15 14:46:13 +0100

    Application initiative NodeJs

---
+++ app.js
@@ -0,0 +1,12 @@
+require('babel-register');
+// installer dans cmd:   npm install --save babel-register
+const os=require('os');
+const fs=require('fs');
+
+console.log('Hellooo World');
+console.log("Est ce que le nodemon marche toujours ? ");
+
+//Utilisation du module os
+console.log(os.arch());
+console.log(os.homedir());
+
